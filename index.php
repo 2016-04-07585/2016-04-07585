2162da8ba86805829e04c6f198e4071d2ade45a4
xxGreyscale <salim.nassor.sn@gmail.com>
2017-06-13 12:45:50 +0300

    ...

---
+++ index.php
@@ -0,0 +1,449 @@
+<html>
+
+<head>
+
+<title> PHOTOSHOOT </title>
+
+<link href="suedish_photoshoot.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body style="background-image: url(FullSizeRender_17.jpg); background-size: 1500px 2000px  ; background-repeat: no-repeat;">
+<!--HEADING -->
+<div>
+<h1> WELCOME TO SUEDISH PHOTOSHOOTS </h1>
+<!-- LOGO -->
+<div id = "logo" style = " position: absolute; right: 0px; top: 20px;">
+logogogogogoog
+
+</div>
+</div>
+
+<div>
+<h3> Recent added shots </h3>
+</div>
+<!-- THE TOP RECENT GALLERY -->
+<div class ="container">
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_4.jpg">
+    <img src="FullSizeRender_4.jpg" alt="Fjords" width="300" height="200">
+  </a>
+  <div class="desc">What you want for our wedding</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_13.jpg">
+    <img src="FullSizeRender_13.jpg" alt="Forest" width="300" height="200">
+  </a>
+  <div class="desc">new trends in  fashion</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_24.jpg">
+    <img src="FullSizeRender_24.jpg" alt="Northern Lights" width="300" height="200">
+  </a>
+  <div class="desc">muscles</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_21.jpg">
+    <img src="FullSizeRender_21.jpg" alt="Mountains" width="300" height="200">
+  </a>
+  <div class="desc">kids wedding shoots</div>
+</div>
+</div>
+<br>
+
+
+
+
+
+<!-- menu navigation -->
+
+<div class = "thelistcontainer"> 
+<ul> 
+<li class = "list"> <a href=# > Wedding shots <a/> </li>
+<li class = "list"> <a href=# > Fashion shots <a/> </li>
+<li class = "list"> <a href=# > Fitness shots <a/> </li>
+<li class = "list"> <a href=# > Kids shots <a/> </li>
+<li class = "list"> <a href=# > Random shots <a/> </li>
+</div>
+
+
+
+<div class = "row">
+<div class="col-0">
+</br> </br></br></br></br></br></br></br></br></br>
+  <ul>
+    <li><span class="tab" onclick="currentSlide(1)">Wedding shots </span></li>
+    <li><span class="tab" onclick="currentSlide(2)">Fashion shots </span></li>
+    <li><span class="tab" onclick="currentSlide(3)">Fitness shots<span></li>
+    <li><span class="tab" onclick="currentSlide(4)">Kids shots </span></li> 
+	<li><span class="tab" onclick="currentSlide(5)">Random shots </span></li>
+  </ul>
+</div>
+</br></br></br></br>
+
+<!-- WEDDING SHOOTS ELEMENT -->
+<div class = "col-4" style = "background-color: grey;">
+
+
+
+
+<div>
+
+</br>
+</br>
+<input type = "text" class= "searchbox">
+<input type="button" class="button1" value="Comment">
+</br></br>
+</div>
+</br></br>
+
+<div class = "co4a" style = "background-color: grey;">
+
+<div class = "titleshoot">
+<p> Wedding Shots </p>
+</div>
+</br> </br> </br>
+
+<div class ="container2">
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_10.jpg">
+    <img src="FullSizeRender_10.jpg" alt="Fjords" width="300" height="200">
+  </a>
+  <div class="desc">happy bride</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_11.jpg">
+    <img src="FullSizeRendr_11.jpg" alt="Forest" width="300" height="200">
+  </a>
+  <div class="desc">suitable background</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_3.jpg">
+    <img src="FullSizeRender_3.jpg"alt="Northern Lights" width="300" height="200">
+  </a>
+  <div class="desc">you can hold flowers too</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_2.jpg">
+    <img src="FullSizeRender_2.jpg" alt="Mountains" width="300" height="200">
+  </a>
+  <div class="desc">twilight </div>
+</div>
+</div>
+<br>
+
+</div>
+
+
+
+</div>
+<!-- FASHION SHOOTS -->
+<div class = "col-4" style = "background-color: grey;">
+
+
+
+
+<div>
+
+</br>
+</br>
+<input type = "text" class= "searchbox">
+<input type="button" class="button1" value="Comment">
+</br></br>
+</div>
+</br></br>
+
+
+<div class = "co4a" style = "background-color: grey;">
+
+<div class = "titleshoot">
+<p> Fashion Shots </p>
+</div>
+</br> </br> </br>
+
+<div class ="container2">
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_8.jpg">
+    <img src="FullSizeRender_8.jpg" alt="Fjords" width="300" height="200">
+  </a>
+  <div class="desc">the love for fashion</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_1.jpg">
+    <img src="FullSizeRender_1.jpg" alt="Forest" width="300" height="200">
+  </a>
+  <div class="desc">something about outdoor</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender.jpg">
+    <img src="FullSizeRender.jpg" alt="Northern Lights" width="300" height="200">
+  </a>
+  <div class="desc">what colour grading is more favorable</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_12.jpg">
+    <img src="FullSizeRender_12.jpg" alt="Mountains" width="300" height="200">
+  </a>
+  <div class="desc">masaai are still on trending</div>
+</div>
+</div>
+<br>
+
+</div>
+
+
+
+</div>
+<!--FITNESS SHOOTS -->
+<div class = "col-4" style = "background-color: grey;">
+
+
+
+
+<div>
+
+</br>
+</br>
+<input type = "text" class= "searchbox">
+<input type="button" class="button1" value="Comment">
+</br></br>
+</div>
+</br></br>
+
+
+<div class = "co4a" style = "background-color: grey;">
+
+<div class = "titleshoot">
+<p> Fitness Shots </p>
+</div>
+</br> </br> </br>
+
+<div class ="container2">
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_24.jpg">
+    <img src="FullSizeRender_24.jpg" alt="Fjords" width="300" height="200">
+  </a>
+  <div class="desc">You want to show more about biceps and triceps</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_25.jpg">
+    <img src="FullSizeRender_25.jpg" alt="Forest" width="300" height="200">
+  </a>
+  <div class="desc">crazy fitness shoot</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_26.jpg">
+    <img src="FullSizeRender_26.jpg" alt="Northern Lights" width="300" height="200">
+  </a>
+  <div class="desc">hold something to show more muscles</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_24.jpg">
+    <img src="FullSizeRender_24.jpg" alt="Mountains" width="300" height="200">
+  </a>
+  <div class="desc">make sure your abs are clearly seen</div>
+</div>
+</div>
+<br>
+
+</div>
+
+
+
+</div>
+<!--KIDS SHOOTS -->
+<div class = "col-4" style = "background-color: grey;">
+
+
+
+
+<div>
+
+</br>
+</br>
+<input type = "text" class= "searchbox">
+<input type="button" class="button1" value="Comment">
+</br></br>
+</div>
+</br></br>
+
+
+<div class = "co4a" style = "background-color: grey;">
+
+<div class = "titleshoot">
+<p> Kids Shots </p>
+</div>
+</br> </br> </br>
+
+<div class ="container2">
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_18.jpg">
+    <img src="FullSizeRender_18.jpg" alt="Fjords" width="300" height="200">
+  </a>
+  <div class="desc">playing kids</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_19.jpg">
+    <img src="FullSizeRender_19.jpg" alt="Forest" width="300" height="200">
+  </a>
+  <div class="desc">Are they in love</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_20.jpg">
+    <img src="FullSizeRender_20.jpg" alt="Northern Lights" width="300" height="200">
+  </a>
+  <div class="desc">outfit matching</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_21.jpg">
+    <img src="FullSizeRender_21.jpg" alt="Mountains" width="300" height="200">
+  </a>
+  <div class="desc">Happy kids</div>
+</div>
+</div>
+<br>
+
+</div>
+
+
+
+</div>
+<!--RANDOM SHOOTS-->
+<div class = "col-4" style = "background-color: grey;">
+
+
+
+
+<div>
+
+</br>
+</br>
+<input type = "text" class= "searchbox">
+<input type="button" class="button1" value="Comment">
+</br></br>
+</div>
+</br></br>
+
+
+<div class = "co4a" style = "background-color: grey;">
+
+<div class = "titleshoot">
+<p> Random Shots </p>
+</div>
+</br> </br> </br>
+
+<div class ="container2">
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_15.jpg">
+    <img src="FullSizeRender_15.jpg" alt="Fjords" width="300" height="200">
+  </a>
+  <div class="desc">In the market</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_23.jpg">
+    <img src="FullSizeRender_23.jpg" alt="Forest" width="300" height="200">
+  </a>
+  <div class="desc">Urban tyle</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_16.jpg">
+    <img src="FullSizeRender_16.jpg" alt="Northern Lights" width="300" height="200">
+  </a>
+  <div class="desc">Couples</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="FullSizeRender_14.jpg">
+    <img src="FullSizeRender_14.jpg" alt="Mountains" width="300" height="200">
+  </a>
+  <div class="desc">Couples on trending</div>
+</div>
+</div>
+<br>
+
+</div>
+
+
+
+</div>
+
+<!-- the left flank -->
+<div class = " col-0" style = "background-color: grey;">
+<h3> Random Shots </h3>
+
+</br></br></br></br></br></br>
+<h3> Calender </h3>
+ <br>
+
+ <ul>
+ <li> most veiwed </li>
+ <li>Most Downloaded </li>
+ <li>Top 25 Shoots </li>
+ </ul>
+ 
+ </br></br>
+ 
+ <input type = "text" class= "commentbox"> 
+ <input type="button" class="button" value="Comment">
+ 
+ </br> </br></br></br></br></br></br></br></br></br> </br></br>
+ 
+ <img src="#" alt="ads" class = "ads">
+</div>
+
+
+
+</div>
+
+
+<!-- The JAVASCRIPT -->
+<script>
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("col-4");
+  var tabs = document.getElementsByClassName("tab");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < tabs.length; i++) {
+      tabs[i].className = tabs[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  tabs[slideIndex-1].className += " active";
+}
+
+</script>
+
+</body>
+</html>
